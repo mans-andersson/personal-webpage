d4befb550b5ef9622fae1229d17c369f8fa26a83
Måns Andersson <mans.andersson97@gmail.com>
2024-11-15 11:39:57 +0100

    Create CareerEntry component

---
+++ src/components/CareerEntry.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  let {
+    companyName,
+    companyLink,
+    companyLogo,
+    jobTitle,
+    timeFrame,
+    backgroundColor = "#0000",
+  }: {
+    companyName: string;
+    companyLink: string;
+    companyLogo: string;
+    jobTitle: string;
+    timeFrame: string;
+    backgroundColor?: string;
+  } = $props();
+</script>
+
+<a
+  class="flex gap-1 justify-between group-hover:opacity-50 hover:!opacity-100"
+  href={companyLink}
+  target="_blank"
+>
+  <img
+    src={companyLogo}
+    alt="{companyName} logo"
+    class="rounded-full p-1 my-auto mr-1"
+    style="background-color: {backgroundColor}"
+    width="48px"
+    height="48px"
+  />
+  <div class="flex flex-col flex-grow py-5 h-full">
+    <span class="text-gray-800 font-semibold flex-grow">{jobTitle}</span>
+    <span class="text-gray-600 font-semibold pr-auto">{companyName}</span>
+  </div>
+  <span class="text-gray-600 py-5 pr-8 h-full">{timeFrame}</span>
+</a>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,101 +1,69 @@
+<script>
+  import CareerEntry from "../../components/CareerEntry.svelte";
+</script>
+
 <h1 class="text-gray-800 font-sans font-bold text-3xl">About</h1>
 <p class="text-gray-800 pt-3 text-sm">
-	My name is Måns Andersson. I am a software engineer based in Stockholm, Sweden. I have a passion
-	for technology, I like building cool things, and I am always trying to learn more and improve.<br
-	/>
-	When I am not behind the desk I can be found lifting weights, climbing, skiing or reading.
+  My name is Måns Andersson. I am a software engineer based in Stockholm,
+  Sweden. I have a passion for technology, I like building cool things, and I am
+  always trying to learn more and improve.<br />
+  When I am not behind the desk I can be found lifting weights, climbing, skiing
+  or reading.
 </p>
 <h1 class="text-gray-800 font-sans font-bold text-3xl mt-8">Work</h1>
 <p class="text-gray-800 py-3 text-sm">
-	<!-- 2+ years of professional development expreience.<br /><br /> -->
-	My first software-related job was teaching others how to code. This was a great experience and I enjoy
-	trying to make seemingly complex things easier for others to understand.<br />
-	Now I work as a Full-stack Engineer at Quickbit, building modern payment solutions based on blockchain technology.
+  <!-- 2+ years of professional development expreience.<br /><br /> -->
+  My first software-related job was teaching others how to code. This was a great
+  experience and I enjoy trying to make seemingly complex things easier for others
+  to understand.<br />
+  Now I work as a Full-stack Engineer at Quickbit, building modern payment solutions
+  based on blockchain technology.
 </p>
 <div class="flex flex-col group">
-	<a
-		class="flex gap-1 justify-between group-hover:opacity-50 hover:!opacity-100"
-		href="https://quickbit.com/"
-		target="_blank"
-	>
-		<img
-			src="/qb_logo.svg"
-			alt="Quickbit logo"
-			class="bg-white rounded-full p-1 my-auto mr-1"
-			width="48px"
-			height="48px"
-		/>
-		<div class="flex flex-col flex-grow py-5 h-full">
-			<span class="text-gray-800 font-semibold flex-grow">Full-stack Engineer</span>
-			<span class="text-gray-600 font-semibold pr-auto">Quickbit</span>
-		</div>
-		<span class="text-gray-600 py-5 pr-8 h-full">2024 -</span>
-	</a>
-	<a
-		class="flex gap-1 justify-between group-hover:opacity-50 hover:!opacity-100"
-		href="https://keyfactor.com/"
-		target="_blank"
-	>
-		<img
-			src="/keyfactor_logo.svg"
-			alt="Keyfactor logo"
-			class="bg-[#5F61FF] rounded-full p-1 my-auto mr-1"
-			width="48px"
-			height="48px"
-		/>
-		<div class="flex flex-col flex-grow py-5 h-full">
-			<span class="text-gray-800 font-semibold flex-grow">Software Engineer</span>
-			<span class="text-gray-600 font-semibold pr-auto">Keyfactor</span>
-		</div>
-		<span class="text-gray-600 py-5 pr-8 h-full">2022 - 2024</span>
-	</a>
-	<a
-		class="flex gap-1 justify-between group-hover:opacity-50 hover:!opacity-100"
-		href="https://ericsson.com/"
-		target="_blank"
-	>
-		<img
-			src="/ericsson_logo.svg"
-			alt="Ericsson logo"
-			class="bg-black rounded-full p-1 my-auto mr-1"
-			width="48px"
-			height="48px"
-		/>
-		<div class="flex flex-col flex-grow py-5 h-full">
-			<span class="text-gray-800 font-semibold flex-grow">Summer Intern and Innovation Intern</span>
-			<span class="text-gray-600 font-semibold pr-auto">Ericsson</span>
-		</div>
-		<span class="text-gray-600 py-5 pr-8 h-full">2021 - 2022</span>
-	</a>
-	<a
-		class="flex gap-1 justify-between group-hover:opacity-50 hover:!opacity-100"
-		href="https://kth.se/"
-		target="_blank"
-	>
-		<img
-			src="/kth_logo.svg"
-			alt="KTH logo"
-			class="bg-[#1751a6] rounded-full p-1 my-auto mr-1"
-			width="48px"
-			height="48px"
-		/>
-		<div class="flex flex-col flex-grow py-5 h-full">
-			<span class="text-gray-800 font-semibold flex-grow">Teaching assistant</span>
-			<span class="text-gray-600 font-semibold pr-auto">KTH Royal Institute of Technology</span>
-		</div>
-		<span class="text-gray-600 py-5 pr-8 h-full">2019</span>
-	</a>
+  <CareerEntry
+    companyName="Quickbit"
+    companyLink="https://quickbit.com/"
+    companyLogo="/qb_logo.svg"
+    jobTitle="Full-stack Engineer"
+    timeFrame="2024 -"
+  />
+  <CareerEntry
+    companyName="Keyfactor"
+    companyLink="https://keyfactor.com/"
+    companyLogo="/keyfactor_logo.svg"
+    jobTitle="Software Engineer"
+    timeFrame="2022 - 2024"
+    backgroundColor="#5F61FF"
+  />
+  <CareerEntry
+    companyName="Ericsson"
+    companyLink="https://ericsson.com/"
+    companyLogo="/ericsson_logo.svg"
+    jobTitle="Summer Intern and Innovation Intern"
+    timeFrame="2021 - 2022"
+    backgroundColor="#000000"
+  />
+  <CareerEntry
+    companyName="KTH Royal Institute of Technology"
+    companyLink="https://kth.se/"
+    companyLogo="/kth_logo.svg"
+    jobTitle="Teaching assistant"
+    timeFrame="2019"
+    backgroundColor="#1751a6"
+  />
 </div>
 <h1 class="text-gray-800 font-sans font-bold text-3xl mt-8">Skills</h1>
 <p class="text-gray-800 pt-3 text-sm">
-	The majority of professional has been back-end development.
-	I am familiar with writing business logic, designing RESTful endpoints, and working with ORM:s to handle data.
-	Most of by back-end work has been with Java and Python.<br/><br/>
+  The majority of professional has been back-end development. I am familiar with
+  writing business logic, designing RESTful endpoints, and working with ORM:s to
+  handle data. Most of by back-end work has been with Java and Python.<br /><br
+  />
 
-	In my full-stack work, I have also evolved my front-end skills. Working with TypeScript, React and doing A/B testing.
-	The website you are currently on, is built with Svelte and Tailwind.<br/><br/>
+  In my full-stack work, I have also evolved my front-end skills. Working with
+  TypeScript, React and doing A/B testing. The website you are currently on, is
+  built with Svelte and Tailwind.<br /><br />
 
-	From my master's degree in computer science I have a good foundation of CS, math and
-	software engineering knowledge.
-	<br /><br />
+  From my master's degree in computer science I have a good foundation of CS,
+  math and software engineering knowledge.
+  <br /><br />
 </p>
