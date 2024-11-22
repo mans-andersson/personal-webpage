5b7a62e1f60ca612cf247714f623690b48085e25
Måns Andersson <mans.andersson97@gmail.com>
2024-11-22 11:09:06 +0100

    Format and text color update

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,23 +1,30 @@
 <h1 class="text-gray-800 font-sans font-bold text-3xl">Projects</h1>
 <h2 class="text-gray-600 font-sans font-bold text-lg pb-5">
-	Here are some projects I've worked on
+  Here are some projects I've worked on
 </h2>
 <a
-	href="https://github.com/Keyfactor/ejbca-ce"
-	target="_blank"
-	class="flex text-lg text-gray-800 font-semibold items-start group"
+  href="https://github.com/Keyfactor/ejbca-ce"
+  target="_blank"
+  class="flex text-lg text-gray-800 font-semibold items-start group"
 >
-	<img src="/ejbca_logo.png" alt="EJBCA logo" class="rounded-xl w-32 h-16 p-3 bg-gray-800 mr-3" />
-	<div class="flex flex-col">
-		<span class="text-gray-800 font-semibold">EJBCA</span>
-		<p class="text-gray-600 text-sm group-hover:hidden">The open-source Certificate Authority</p>
-		<p class="text-gray-800 text-sm group-hover:block hidden">
-			EJBCA is one of the longest running CA software projects, providing time-proven robustness,
-			reliability and flexibitlity. EJBCA is platform independent and can easily be scaled out to
-			match the needs of your PKI requirements. EJBCA is based on Java and Jakarta and runs in a
-			Wildfly application server, but is also available as a container. I have worked on EJBCA since
-			2022, EJBCA Community Edition is open-source and available on Github. EJBCA Enterprise Edition
-			is available only to paying customers.
-		</p>
-	</div>
+  <img
+    src="/ejbca_logo.png"
+    alt="EJBCA logo"
+    class="rounded-xl w-32 h-16 p-3 bg-gray-800 mr-3"
+  />
+  <div class="flex flex-col">
+    <span class="text-gray-800 font-semibold">EJBCA</span>
+    <p class="text-gray-600 text-sm group-hover:hidden">
+      The open-source Certificate Authority
+    </p>
+    <p class="text-gray-600 text-sm group-hover:block hidden">
+      EJBCA is one of the longest running CA software projects, providing
+      time-proven robustness, reliability and flexibitlity. EJBCA is platform
+      independent and can easily be scaled out to match the needs of your PKI
+      requirements. EJBCA is based on Java and Jakarta and runs in a Wildfly
+      application server, but is also available as a container. I have worked on
+      EJBCA since 2022, EJBCA Community Edition is open-source and available on
+      Github. EJBCA Enterprise Edition is available only to paying customers.
+    </p>
+  </div>
 </a>
