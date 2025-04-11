80923389ece143b774660d237dde31f8f07fa54e
Måns Andersson <mans.andersson97@gmail.com>
2025-04-11 14:23:15 +0200

    Separate project component

---
+++ src/components/MacWindow.svelte
@@ -0,0 +1,10 @@
+<div class="rounded-xl border border-gray-300 overflow-hidden shadow-md">
+  <div class="bg-gray-200 h-6 px-3 flex items-center space-x-2">
+    <div class="w-3 h-3 bg-red-500 hover:bg-red-800 rounded-full"></div>
+    <div class="w-3 h-3 bg-yellow-500 hover:bg-yellow-800 rounded-full"></div>
+    <div class="w-3 h-3 bg-green-500 hover:bg-green-800 rounded-full"></div>
+  </div>
+  <div class="bg-white p-4">
+    <slot />
+  </div>
+</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,19 +1,34 @@
+<script>
+  import GithubLink from "../../components/GithubLink.svelte";
+  import MacWindow from "../../components/MacWindow.svelte";
+  import WebLink from "../../components/WebLink.svelte";
+</script>
+
 <h1 class="text-gray-800 font-sans font-bold text-3xl">Projects</h1>
 
 <div class="flex flex-col gap-4 py-4">
-  <div class="rounded-xl border border-gray-300 overflow-hidden w-96 shadow-md">
-    <div class="bg-gray-200 h-6 px-3 flex items-center space-x-2">
-      <div class="w-3 h-3 bg-red-500 rounded-full"></div>
-      <div class="w-3 h-3 bg-yellow-500 rounded-full"></div>
-      <div class="w-3 h-3 bg-green-500 rounded-full"></div>
+  <MacWindow>
+    <div class="flex items-start gap-4">
+      <div class="flex flex-col gap-2">
+        <img
+          src="/ejbca_logo.png"
+          alt="Project 1"
+          class="p-3 bg-gray-500 rounded-lg min-w-28 w-28"
+        />
+        <div class="pl-2">
+          <GithubLink
+            text="Github"
+            href="https://github.com/Keyfactor/ejbca-ce"
+          />
+          <WebLink text="Website" href="https://www.ejbca.org/" />
+        </div>
+      </div>
+      <p class="text-gray-650 text-sm border-l-2 border-gray-300 pl-4">
+        EJBCA is a widely used open-source PKI software that many large
+        organizations use for their PKIs. I contributed to the project through
+        my work at Keyfactor. A highlight of my work was heading the project to
+        migrate from Ant to Gradle, resulting in build times reducing by 75%.
+      </p>
     </div>
-    <div class="bg-white p-4 flex gap-4">
-      <img
-        src="/ejbca_logo.png"
-        alt="Project 1"
-        class="p-3 bg-gray-500 rounded-lg"
-      />
-      <p>EJBCA is a widely used open source PKI software that many large companies use for their PKIs.</p>
-    </div>
-  </div>
+  </MacWindow>
 </div>
